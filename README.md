ac0cccac6a684b3896455d32fc02ada8e63bf9a8
eavanvalkenburg <github@vanvalkenburg.eu>
2024-12-05 11:30:18 +0100

    updated code and readme

--- README.md
+++ README.md
@@ -17,10 +17,23 @@ Go to Internet/WAN settings, under Dynamics DNS:
 2. Hostname: the format is `name.zone`, and it can include '@' as the name, for instance `@.azure.com`
 3. Username: the username you set in the function app
 4. Password: the password you set in the function app
-5. Server: the function URL you get after deployment, without https and with a `?` at the end, so `functionname.azurewebsites.net/api/update_dns?`
+5. Server: the function URL you get after deployment, without https and with a `?` at the end, so `functionname.azurewebsites.net/api/update_dns?hostname=%h%myip=%i`
 
 Test and see!
+To force a test, ssh into your Unifi Controller and run:
+`ps aux | grep inadyn`
+This should show some services running, including a config file name, something like: `/run/ddns-eth4.300-inadyn.conf`
 
-If you ssh into your Unify controller, you can see the logs by running `cat /var/log/messages | grep inadyn`
+Then run: `cat /run/ddns-eth4.300-inadyn.conf` to verify that your settings are correct.
 
-This should also work with otehr Unify systems and potentially other devices that support dynamic DNS. It is based on: [this blog post](https://blog.nielsb.net/dynamic-dns-with-edgerouter-and-azure) by [Niels Buit](https://github.com/nielsams).
+Finally run: `/usr/sbin/inadyn -n -s -C -f /run/ddns-eth4.300-inadyn.conf -1 -l debug --foreground`
+
+With the name of your config, this should force the update and should result in either:
+1. `nochg <ip address>`
+2. `good <ip address>`
+
+Then all is good!
+
+While running you can also check the logs by running `cat /var/log/messages | grep inadyn`
+
+This should also work with other Unify systems and potentially other devices that support dynamic DNS. It is based on: [this blog post](https://blog.nielsb.net/dynamic-dns-with-edgerouter-and-azure) by [Niels Buit](https://github.com/nielsams).
--- function_app.py
+++ function_app.py
@@ -1,20 +1,38 @@
 """Main function that runs when called with http request."""
+
+import asyncio
 import logging
 import os
 
 import azure.functions as func
 from azure.core.exceptions import HttpResponseError
-from azure.identity.aio import DefaultAzureCredential
-from azure.mgmt.dns.aio import DnsManagementClient
-from azure.mgmt.dns.v2018_05_01.models import ARecord, RecordSet
+from azure.identity.aio import ManagedIdentityCredential
+from azure.mgmt.dns.v2023_07_01_preview.aio import DnsManagementClient
+from azure.mgmt.dns.v2023_07_01_preview.models import (
+    ARecord,
+    AaaaRecord,
+    RecordSet,
+    RecordType,
+)
+
+logger = logging.getLogger(__name__)
 
 app = func.FunctionApp()
 
-SUCCESS = "good"
+SUCCESS = "good {}"
+NOCHANGE = "nochg {}"
 UNAUTHORIZED = "badauth"
-UPDATE_ERROR = "dnserr"
+UPDATE_ERROR = "911"
 NOZONE = "nohost"
-NOCHANGE = "nochg"
+
+HEADER_AUTH = "Authorization"
+PARAM_HOSTNAME = "hostname"
+PARAM_IP = "myip"
+PARAM_IPV6 = "myipv6"
+
+UPDATER_CREDENTIAL = "UpdaterCredential"
+RESOURCE_GROUP = "ResourceGroup"
+SUBSCRIPTION_ID = "SubscriptionId"
 
 
 @app.function_name(name="dyndns")
@@ -25,95 +43,210 @@ NOCHANGE = "nochg"
 )
 async def update_dns(req: func.HttpRequest) -> func.HttpResponse:
     """Code that reads the query and processes the dns changes."""
-    logging.info("Request: %s", req)
-
-    auth = req.headers.get("Authorization")
+    logger.info("Requested url: %s", req.url)
+    auth = req.headers.get(HEADER_AUTH)
     if auth is None:
-        logging.info("Unauthorized request, no auth header")
+        logger.warning("Unauthorized request, no auth header")
         return func.HttpResponse(UNAUTHORIZED)
-    basic_auth = auth.split(" ")[1]
-    credential = basic_auth.encode()
-    logging.info("Credential: %s", credential)
-    if credential != os.getenv("UpdaterCredential"):
-        logging.info("Unauthorized request, incorrect credentials")
+    stored_auth = os.environ[UPDATER_CREDENTIAL]
+    if not stored_auth.startswith("Basic "):
+        stored_auth = f"Basic {stored_auth}"
+    if auth != stored_auth:
+        logger.warning("Unauthorized request, incorrect credentials")
         return func.HttpResponse(UNAUTHORIZED)
 
-    logging.warning("Request: %s", req)
-    logging.warning("Route params: %s", req.route_params)
-    # query = req.route_params.get("query")
+    hostname = req.params.get(PARAM_HOSTNAME)
+    ip_address = req.params.get(PARAM_IP)
+    ipv6_address = req.params.get(PARAM_IPV6)
 
-    hostname = req.route_params.get("hostname")
-    ip_address = req.route_params.get("myip")
+    if not hostname or not (ip_address or ipv6_address):
+        logger.warning("Missing hostname or ip address")
+        logger.warning("Params: %s", req.params)
+        logger.warning("Headers: %s", req.headers.__dict__)
+        logger.warning("body: %s", req.get_body())
+        logger.warning("route_params: %s", req.route_params)
+        logger.warning("req: %s", req.__dict__)
+        return func.HttpResponse(UPDATE_ERROR, status_code=500)
 
-    if not hostname or not ip_address:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            hostname = req_body.get("hostname")
-            ip_address = req_body.get("myip")
-
-    if not hostname or not ip_address:
-        logging.info("Missing hostname or ip address")
-        return func.HttpResponse(UPDATE_ERROR)
+    if ip_address and "," in ip_address and not ipv6_address:
+        ipv6_address = ip_address.split(",")[1]
+        ipv4_address = ip_address.split(",")[0]
+    else:
+        ipv4_address = ip_address
 
     try:
-        changed = True
-        # changed = await update_dns_records(hostname, ip_address)
+        results = await update_dns_records(hostname, ipv4_address, ipv6_address)
     except ZoneException:
-        logging.error("Zone not found")
         return func.HttpResponse(NOZONE)
     except HttpResponseError as exc:
-        logging.error("Other error in request, %s", exc)
-        return func.HttpResponse(UPDATE_ERROR)
-    return func.HttpResponse(SUCCESS if changed else NOCHANGE)
+        logger.error("Other error in request, %s", exc)
+        return func.HttpResponse(UPDATE_ERROR, status_code=500)
+    if ipv4_address:
+        if ipv6_address:
+            ip_address = f"{ipv4_address},{ipv6_address}"
+        else:
+            ip_address = ipv4_address
+    else:
+        ip_address = ipv6_address
+    response = [
+        SUCCESS.format(ip_address) if res else NOCHANGE.format(ip_address)
+        for res in results
+    ]
+    return func.HttpResponse("\n".join(response))
 
 
-async def update_dns_records(hostname: str, ip_address: str) -> bool:
+async def update_dns_records(
+    hostnames: str, ipv4_address: str | None, ipv6_address: str | None
+) -> list[bool]:
     """Update the dns records for the hostname."""
-    # Replace this with your subscription id
-    subscription_id = os.environ["SubscriptionId"]
-    resource_group = os.environ["ResourceGroup"]
+    rg = os.environ[RESOURCE_GROUP]
+    subscription = os.environ[SUBSCRIPTION_ID]
+    logger.info(
+        f"Updating dns records for {hostnames} with ipv4: {ipv4_address}, and ipv6 {ipv6_address} in rg: {rg} and subscription: {subscription}"
+    )
+    try:
+        async with (
+            ManagedIdentityCredential() as cred,
+            DnsManagementClient(
+                credential=cred,
+                subscription_id=subscription,
+            ) as dns,
+        ):
+            zones = dns.zones.list_by_resource_group(resource_group_name=rg)
+            async for zone in zones:
+                logger.debug("Zone: %s", zone)
+            return await asyncio.gather(
+                *[
+                    run_check_and_update(
+                        dns,
+                        rg,
+                        hostname,
+                        ipv4_address,
+                        ipv6_address,
+                    )
+                    for hostname in hostnames.split(",")
+                ]
+            )
+    except Exception as exc:
+        logger.error("Error updating dns records: %s", exc)
+        raise
+    finally:
+        await cred.__aexit__(None, None, None)
 
-    # split hostname into zone and record name
+
+async def run_check_and_update(
+    dns: DnsManagementClient,
+    rg: str,
+    hostname: str,
+    ipv4_address: str | None,
+    ipv6_address: str | None,
+) -> bool:
+    """Check for the record and update if needed."""
     record_name = hostname.split(".", 1)[0]
     zone_name = hostname.split(".", 1)[1]
+    try:
+        await dns.zones.get(rg, zone_name)
+    except HttpResponseError as exc:
+        raise ZoneException(f"Zone not found: {zone_name}") from exc
 
-    async with DefaultAzureCredential() as credential:
-        async with DnsManagementClient(
-            credential=credential,
-            subscription_id=subscription_id,
-        ) as dns_client:
-            # get the zone
-            zone = await dns_client.zones.get(
-                resource_group_name=resource_group, zone_name=zone_name
+    changed = False
+    if ipv4_address is not None:
+        to_update = await record_to_update(
+            dns, rg, zone_name, record_name, ipv4_address
+        )
+        if to_update:
+            changed = await update_record(dns, rg, zone_name, record_name, ipv4_address)
+    if ipv6_address is not None:
+        to_update = await record_to_update(
+            dns,
+            rg,
+            zone_name,
+            record_name,
+            ipv6_address,
+            ipv6_flag=True,
+        )
+        if to_update:
+            changed = await update_record(
+                dns,
+                rg,
+                zone_name,
+                record_name,
+                ipv6_address,
+                ipv6_flag=True,
             )
-            if not zone:
-                # zone doesn't exist, unable to update
-                raise ZoneException("Zone not found")
-            record = await dns_client.record_sets.get(
-                resource_group_name=resource_group,
-                zone_name=zone_name,
-                relative_record_set_name=record_name,
-                record_type="A",
-            )
+    return changed
+
+
+async def update_record(
+    dns: DnsManagementClient,
+    rg: str,
+    zone_name: str,
+    record_name: str,
+    ip_address: str,
+    ipv6_flag: bool = False,
+) -> bool:
+    """Update the record."""
+    if ipv6_flag:
+        record_set = RecordSet(
+            ttl=3600, aaaa_records=[AaaaRecord(ipv6_address=ip_address)]
+        )
+    else:
+        record_set = RecordSet(ttl=3600, a_records=[ARecord(ipv4_address=ip_address)])
+    logger.debug(
+        "Updating record %s for zone %s and ip %s",
+        record_name,
+        zone_name,
+        ip_address,
+    )
+    await dns.record_sets.create_or_update(
+        resource_group_name=rg,
+        zone_name=zone_name,
+        relative_record_set_name=record_name,
+        record_type=RecordType.AAAA if ipv6_flag else RecordType.A,
+        parameters=record_set,
+    )
+    return True
+
+
+async def record_to_update(
+    dns: DnsManagementClient,
+    rg: str,
+    zone_name: str,
+    record_name: str,
+    ip_address: str | None,
+    ipv6_flag: bool = False,
+) -> bool:
+    """Check if the record needs to be updated."""
+    try:
+        logger.debug(
+            "Checking record %s for zone %s and ip %s",
+            record_name,
+            zone_name,
+            ip_address,
+        )
+        record = await dns.record_sets.get(
+            resource_group_name=rg,
+            zone_name=zone_name,
+            relative_record_set_name=record_name,
+            record_type=RecordType.AAAA if ipv6_flag else RecordType.A,
+        )
+        logger.debug("Record: %s", record)
+        if ipv6_flag:
             if (
-                record
-                and record.a_records is not None
-                and record.a_records[0].ipv4_address == ip_address
+                record.aaaa_records is not None
+                and record.aaaa_records[0].ipv6_address != ip_address
             ):
-                return False
-            await dns_client.record_sets.create_or_update(
-                resource_group_name=resource_group,
-                zone_name=zone_name,
-                relative_record_set_name=record_name,
-                record_type="A",
-                parameters=RecordSet(
-                    ttl=3600, a_records=[ARecord(ipv4_address=ip_address)]
-                ),
-            )
+                return True
+            return False
+        if (
+            record.a_records is not None
+            and record.a_records[0].ipv4_address != ip_address
+        ):
             return True
+        return False
+    except HttpResponseError:
+        logger.info("Record not found, creating")
+        return True
 
 
 class ZoneException(Exception):
